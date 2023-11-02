068d8ad4de980caa7b5b8fe538e0b64b1f2dce33
adwaitpawar <110279221+adwaitpawar@users.noreply.github.com>
2023-11-02 16:28:20 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_key_pair" "mykeypair" {
+  key_name = "my-keypair"
+  public_key = file("my-key.pub")
+}
+
+resource "aws_instance" "myinstance" {
+  ami = "ami-0447a12f28fddb066"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.mykeypair.key_name
+
+  tags = {
+    Name = "my-ec2-instance"
+  }
+}
